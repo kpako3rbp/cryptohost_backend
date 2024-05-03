56dc3b59f2fae4a64c9202d83485eb193dc4b60f
kpako3rbp <hansss@list.ru>
2024-05-04 01:44:19 +0400

    change baseUrl

--- client/routes.js
+++ client/routes.js
@@ -1,8 +1,8 @@
 // export const baseUrl = 'http://192.168.1.56:8000'; // TODO поменять URL
-// export const baseUrl = process.env.API_URL || 'http://95.181.226.159:8000';
+export const baseUrl = process.env.API_URL || 'http://95.181.226.159:8000';
 // export const baseUrl = 'http://192.168.1.56:8000';
 
-export const baseUrl = process.env.API_URL;
+// export const baseUrl = process.env.API_URL;
 
 // export const baseUrl = 'http://192.168.1.56:8000';
 // export const baseUrl = 'http://95.181.226.159:8000';
