352ad2fee3f522c417122916a55cfb59a6acbbc6
kpako3rbp <hansss@list.ru>
2024-05-04 17:18:40 +0400

    add env variables

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
+  env: {
+    SERVER_URL: process.env.API_URL,
+    CLIENT_DOMAIN: process.env.CLIENT_DOMAIN,
+  }
 };
 
 export default nextConfig;
